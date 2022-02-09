1769a46f82bf6df6294dc6c613bb50e9def7b355
hiwad rashad <hiwadrashad112@hotmail.com>
2022-02-09 16:02:16 +0100

    Add project files.

---
+++ HTML-Custom-Made-Frontpage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTML_Custom_Made_Frontpage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
